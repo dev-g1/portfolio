9dcc9a490942a83aceca60d20252cca1eef249a2
Golden <irokagolden@gmail.com>
2022-09-08 05:40:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let num = 10 + 10 * 3;
+console.log(num);
+
+
+function Golden() {
+    document.getElementById("show").innerHTML = "Thank you for reaching out. We will get back to you as soon as possible. God Bless you.";
+}
